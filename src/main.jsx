fe081fcf90e6d5739459b8e50fabb77ce57e859c
Levi Frank <LeviOrionFrank@gmail.com>
2022-07-06 14:00:14 -0400

    just finished lesson 7

---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// --------------------------------------------------------------------------------------
+// 5.
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <ul>
+//         <li>Thing 1</li>
+//         <li>Thing 2</li>
+//         <li>Thing 4</li>
+//     </ul>
+// )
+// ReactDOM.createRoot(<ul><li>Thing 1</li> <li>Thing 2</li></ul>)
+
+
+// --------------------------------------------------
+// 6.
+
+
+// function Blah() {
+//     return(
+//         <h1>I am a cow mooooooo</h1>
+//     )
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//             <div>
+//                 <Blah />
+//             </div>
+// )
+
+
+// --------------------------------------------------
+// 7.
+
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+//                   =
+// const h1 = document.createElement("h1")
+// h1.textContent = "Blah"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+
+// --------------------------------------------------
+// 8.
