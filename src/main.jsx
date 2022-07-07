a3fd5cabd6bcccaa1997974d69e4a3da44e5a2e8
Levi Frank <LeviOrionFrank@gmail.com>
2022-07-07 16:29:23 -0400

    done with challenge 12

--- src/main.jsx
+++ src/main.jsx
@@ -1,86 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import LogoSvg from "./logo.svg"
 
-// --------------------------------------------------------------------------------------
-// 5.
-
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//     <ul>
-//         <li>Thing 1</li>
-//         <li>Thing 2</li>
-//         <li>Thing 4</li>
-//     </ul>
-// )
-// ReactDOM.createRoot(<ul><li>Thing 1</li> <li>Thing 2</li></ul>)
-
-
-// --------------------------------------------------
-// 6.
-
-
-// function Blah() {
-//     return(
-//         <h1>I am a cow mooooooo</h1>
-//     )
-// }
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//             <div>
-//                 <Blah />
-//             </div>
-// )
-
-
-// --------------------------------------------------
-// 7.
-
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
-//                   =
-// const h1 = document.createElement("h1")
-// h1.textContent = "Blah"
-// h1.className = "header"
-// document.getElementById("root").append(h1)
-
-
-// --------------------------------------------------
-// 8.
-
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
-
-// const element = (
-//     <div> 
-//         <h1 className="header">This is JSX</h1>
-//         <p>blahblahblah</p> 
-//     </div>)
-// console.log(element)
-
-// const navbar = (
-//     <nav>
-//         <h1>Levi's Scrumptious Chocolate!</h1>
-//         <ul>
-//             <li>Pricing</li>
-//             <li>About</li>
-//             <li>Contact</li>
-//         </ul>
-//     </nav>
-// )
-
-
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//             <div>
-//                 <Blah />
-//             </div>
-// )
-
-// ---------------------------------------------------
-// 9.
-
-// const samplePage = (
+// const samePage = (
 //     <nav>
 //         <h1>My Little Sample Page!</h1>
 //         <h3>Coded By: Levi!</h3>
@@ -104,7 +26,7 @@ import ReactDOM from "react-dom/client"
 
 const factPage = (
     <nav>
-        <img src="./logo.svg" alt="" />
+        <img src={LogoSvg} height="40px" />
         <h1>Fun Facts About React</h1>
         <ul>
             <li>Was First Released in 2013</li>
@@ -119,3 +41,4 @@ const factPage = (
 ReactDOM.createRoot(document.getElementById("root")).render(
     factPage
 )
+
