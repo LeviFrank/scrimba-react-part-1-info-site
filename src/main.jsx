2217c6e75cd6dc337f7760433ad0a03fdad8249a
Levi Frank <LeviOrionFrank@gmail.com>
2022-07-07 18:34:53 -0400

    finished challenge 14

--- src/main.jsx
+++ src/main.jsx
@@ -24,21 +24,35 @@ import LogoSvg from "./logo.svg"
 // ----------------------------------------------------
 // 10.
 
-const factPage = (
-    <nav>
-        <img src={LogoSvg} height="40px" />
-        <h1>Fun Facts About React</h1>
-        <ul>
-            <li>Was First Released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained By FaceBook</li>
-            <li>Powers Thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </nav>
-)
+// const factPage = (
+//     <nav>
+//         <img src={LogoSvg} height="40px" />
+//         <h1>Fun Facts About React</h1>
+//         <ul>
+//             <li>Was First Released in 2013</li>
+//             <li>Was originally created by Jordan Walke</li>
+//             <li>Has well over 100k stars on Github</li>
+//             <li>Is maintained By FaceBook</li>
+//             <li>Powers Thousands of enterprise apps, including mobile apps</li>
+//         </ul>
+//     </nav>
+// )
+
+
+    
+// 13.
+
+function Page() {
+    return (
+    <ol>
+        <li>Make An App</li>
+        <li>I Can Be Pround To Build An App And Be An Advanced Coder</li>
+        <li>MAKE SOME $!!!</li>
+        <li>Cuz My Dad Wants Me To</li>
+    </ol>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    factPage
+    <Page/>
 )
-
